66fce1480c2efc4dee9cae5dd2e92677f67d26c7
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-09 02:41:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+// expect a sourarrat an a function. all the test will pass an array and a block.. remember, map returns a new Array.
+
+function map(sourceArray, callBack) {
+  let array = []
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    let object = sourceArray[i]
+    array.push(callBack(object))
+  }
+  return array;
+}
+
+// should expect a source array and optionaly a starting bale. all the test will pass an array an (sometimes) a starting point.
+function reduce(sourceArray, callBack, starting) {
+  let array = (!!starting) ? starting: sourceArray[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < sourceArray.length; i++) {
+    array = callBack(sourceArray[i], array)
+  }
+  return array;
+}
