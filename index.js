f84ea28a3bbd60209c349f6a05ef7483963ac25e
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-09 02:16:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function map(src, cb) {
 
   return r;
 }
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
