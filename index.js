6e6c133f68778075a40c4df3a0383e747f3d67ca
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-09 02:38:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function map(sourceArray, callBack) {
   }
   return array;
 }
+
+// should expect a source array and optionaly a starting bale. all the test will pass an array an (sometimes) a starting point.
+function reduce(sourceArray, callBack) {
+    let array = []
+
+    return array;
+}
