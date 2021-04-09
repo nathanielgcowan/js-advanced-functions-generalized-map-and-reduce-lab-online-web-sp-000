3bb88cc48fe38f95178e8f6d363cc28277e112b6
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-09 02:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions here
+function map(src, cb) {
+  let r = []
+
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(cb(theElement))
+  }
+
+  return
